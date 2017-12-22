8ec99a830259f117255cc7fceaebf9f85e65c1a6
Zach Mitchell <zmitchell@fastmail.com>
2017-12-22 17:09:24 -0600

    Add requirements to setup.py

--- setup.py
+++ setup.py
@@ -7,18 +7,15 @@ with open('README.me') as readme_file:
     readme = readme_file.read()
 
 requirements = [
-    # TODO: put package requirements here
-]
-
-test_requirements = [
-    'pytest'
-    # TODO: put package test requirements here
+    'selenium>=3.8.0',
+    'requests>=2.18.4',
+    'logzero>=1.3.1',
 ]
 
 setup(
     name='tyt_download_automator',
     version='0.0.0',
-    description="TYT Download Automator automates the process of downloading new episodes for members of the TYT Network",
+    description="TYT Download Automator automates the process of downloading new episodes for members of the TYT Network",  # noqa
     long_description=readme + '\n\n',
     author="Zach Mitchell",
     author_email='zmitchell@fastmail.com',
@@ -39,8 +36,6 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6'
     ],
-    test_suite='tests',
-    tests_require=test_requirements,
     entry_points={
         'console_scripts': [
             'tyt_download_automator = tyt_download_automator.__main__:main'
